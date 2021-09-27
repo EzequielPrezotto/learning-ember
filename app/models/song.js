e0af234ac0d64b8f6b19e08d5fa67bf804dc311c
ezequielprezotto <eprezotto@apple.com>
2021-09-27 15:38:47 -0300

    commit

--- app/models/song.js
+++ app/models/song.js
@@ -1,7 +1,11 @@
+// app/models/song.js
 export default class Song {
-  constructor({ title, rating, band }) {
+  // constructor({ title, rating, band }) {
+  constructor({ id, title, rating, band }, relationships={}) {
+    this.id = id;
     this.title = title;
     this.rating = rating ?? 0;
     this.band = band;
+    this.relationships = relationships;
   }
 }
