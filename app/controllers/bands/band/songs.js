eeefec89494a8c7803a80fc56ba3bf5686331b32
ezequielprezotto <eprezotto@apple.com>
2021-09-28 08:44:16 -0300

    commit

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,9 +1,7 @@
 import Controller from '@ember/controller';
 import {tracked} from '@glimmer/tracking';
 import {action} from '@ember/object';
-import Song from 'rarwee/models/song';
 import {inject as service} from '@ember/service';
-import fetch from 'fetch';
 
 export default class BandsBandSongsController extends Controller {
   @tracked showAddSong = true;
@@ -14,63 +12,68 @@ export default class BandsBandSongsController extends Controller {
   updateTitle(event) {
     this.title = event.target.value;
   }
-  
+
   @action
   async updateRating(song, rating) {
-    song.rating = rating;
-    let payload = {
-      data: {
-        id: song.id,
-        type: 'songs',
-        attributes: {
-          rating
-        }
-      }
-    };
-    await fetch(`/songs/${song.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/vnd.api+json'
-      },
-      body: JSON.stringify(payload)
-    });
+    // song.rating = rating;
+    // let payload = {
+    //   data: {
+    //     id: song.id,
+    //     type: 'songs',
+    //     attributes: {
+    //       rating
+    //     }
+    //   }
+    // };
+    // await fetch(`/songs/${song.id}`, {
+    //   method: 'PATCH',
+    //   headers: {
+    //     'Content-Type': 'application/vnd.api+json'
+    //   },
+    //   body: JSON.stringify(payload)
+    // });
+
+    this.catalog.update('song', song, { rating });
   }
 
   @action
-  // saveSong() {
-  //     let song = new Song({ title: this.title, band: this.model });
   async saveSong() {
-    let payload = {
-      data: {
-        type: 'songs',
-        attributes: { title: this.title },
-        relationships: {
-          band: {
-            data: {
-              id: this.model.id,
-              type: 'bands'
-            }
-          }
-        }
-      }
-    };
-    let response = await fetch('/songs', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/vnd.api+json'
-      },
-      body: JSON.stringify(payload)
-    });
+    // let payload = {
+    //   data: {
+    //     type: 'songs',
+    //     attributes: { title: this.title },
+    //     relationships: {
+    //       band: {
+    //         data: {
+    //           id: this.model.id,
+    //           type: 'bands'
+    //         }
+    //       }
+    //     }
+    //   }
+    // };
+    // let response = await fetch('/songs', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/vnd.api+json'
+    //   },
+    //   body: JSON.stringify(payload)
+    // });
 
-    let json = await response.json();
-    let { id, attributes, relationships } = json.data;
-    let rels = {};
-    for (let relationshipName in relationships) {
-      rels[relationshipName] =
-        relationships[relationshipName].links.related;
-    }
-    let song = new Song({ id, ...attributes }, rels);
-    this.catalog.add('song', song);
+    // let json = await response.json();
+    // let { id, attributes, relationships } = json.data;
+    // let rels = {};
+    // for (let relationshipName in relationships) {
+    //   rels[relationshipName] = relationships[relationshipName].links.related;
+    // }
+    // let song = new Song({ id, ...attributes }, rels);
+    // this.catalog.add('song', song);
+
+    let song = await this.catalog.create(
+      'song',
+      { title: this.title },
+      { band: { data: { id: this.model.id, type: 'bands' } } }
+    );
 
     this.model.songs = [...this.model.songs, song];
     this.title = '';
